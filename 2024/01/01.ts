d1dc52b95fb524cc5ce31d3e530e760e304d2854
Lukáš Marek <lukas.marek@mail.com>
2024-12-01 10:04:46 +0100

    Day 1

--- 2024/01/01.ts
+++ 2024/01/01.ts
@@ -1,6 +1,24 @@
 import readLines from "../../utils/readLines";
+import sum from "../../utils/sum";
 
 const input = readLines("input.txt");
-const result = input;
 
-console.log(result);
+const numbers = input.map((line) => line.split(/\s+/).map((s) => parseInt(s)));
+
+const A = numbers.map(([a]) => a);
+const B = numbers.map(([_, b]) => b);
+
+A.sort();
+B.sort();
+
+const indexes = Array.from(Array(A.length).keys());
+
+const partA = indexes.map((i) => Math.abs(A[i] - B[i])).reduce(sum);
+
+console.log("Part A: ", partA);
+
+const partB = A.map((n) => {
+    return n * B.filter((m) => m == n).length;
+}).reduce(sum);
+
+console.log("Part B: ", partB);
